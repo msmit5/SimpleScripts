f25a245e86ab70abea2fd239987c1fcad16032bd
msmit5 <55892653+msmit5@users.noreply.github.com>
2022-05-23 21:52:31 -0400

    fixed mute because 0% != mute

--- toggleMuteMic.sh
+++ toggleMuteMic.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-# On my laptop, this is the best volume level. Change to fit needs
 SAFE_MIC_LEVEL="25%"
 
 # check mic level
-if [ "$(amixer sget Capture | grep "\w[0-9]%" -o -m1)" = $SAFE_MIC_LEVEL ] ;then
+if [ "$(amixer sget Capture | grep -E "\w[0-9]{0,3}%" -o -m1)" = $SAFE_MIC_LEVEL ] ;then
 
-    # echo Muting mic
+    echo Muting mic
 	# set volume to 0 by subtracting 100%
 	amixer set Capture 100%- -q
+	amixer set Capture toggle
 else
-    # echo raising to safe level
+    echo raising to safe level
 	# set volume to 0 by subtracing 100%
 	# then, set to 25% by increasing it by 25% from 0
+	amixer set Capture toggle
 	amixer set Capture 100%- -q
 	amixer set Capture $SAFE_MIC_LEVEL -q
 fi
